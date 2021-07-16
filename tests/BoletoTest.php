09067038f2c46b209ea18c34d38c8c51da62e67d
Marcos Motta <mrcsmotta1@gmail.com>
2021-07-15 23:18:57 -0300

    Teste de rota inexistente 404

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -2,11 +2,18 @@
 
 declare(strict_types=1);
 
+use Pecee\SimpleRouter\Route\Route;
 use PHPUnit\Framework\TestCase;
 
 final class BoletoTest extends TestCase
 {
   function testeRotaInexistente()
   {
+    $client = new GuzzleHttp\Client();
+    $res = $client->get(URL_BASE . "/erro");
+    print_r($res);
+
+
+    // $this->assertEquals(200, $response->getStatusCode());
   }
 }
