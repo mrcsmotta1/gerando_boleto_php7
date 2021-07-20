6d923a92a4d32fa3af1660c3dceced517ed1c09a
Marcos Motta <mrcsmotta1@gmail.com>
2021-07-19 21:52:05 -0300

    Criando conexão com BD e criando .env_example

--- source/Config.php
+++ source/Config.php
@@ -1,3 +1,18 @@
 <?php
 
 define("URL_BASE", "http://boleto.com");
+
+define("DATA_LAYER_CONFIG", [
+  "driver" => "mysql",
+  "host" => "localhost",
+  "port" => "3306",
+  "dbname" => "bancoBoleto",
+  "username" => "root",
+  "passwd" => "123456789",
+  "options" => [
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    PDO::ATTR_CASE => PDO::CASE_NATURAL
+  ]
+]);
